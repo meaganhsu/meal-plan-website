249cd35dc8b128d44252b1659233306e4b3e9dda
meaganhsu <myth72@student.cis.edu.hk>
2025-09-06 15:01:57 +0800

    .

--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; // Add useEffect
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import '../styles/Authentication.css';
 
